5fe108869271166db66fec201d2c2a9ff1cd1309
AleksandrDev <myshalow_aleksandar@mail.ru>
2024-05-18 17:49:32 +0300

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace ConsoleApp6;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Main начал работать в {Thread.CurrentThread.ManagedThreadId}");
+        Console.WriteLine("Нажмите любую кнопку для старта");
+        Console.ReadKey();
+
+        var threadPoolWorker = new ThreadPoolWorker<int>(SumNumber);
+        threadPoolWorker.Start(1000);
+
+        /*        for (int i = 0; i < 20; i++)
+                {
+                    Console.Write('-');
+                    Thread.Sleep(50);
+                }*/
+
+        Report();
+        var res = threadPoolWorker.Result;
+
+        Console.WriteLine(res);
+    } 
+
+    private static int SumNumber(object number)
+    {
+        int num = (int)number;
+        int sum = 0;
+        for (int i = 0; i < num; i++)
+        {
+            sum += i;
+            Thread.Sleep(10);
+        }
+        return sum;
+    }
+
+    private static void Example1(object arg)
+    {
+        Console.WriteLine($"Example1 начал работать в {Thread.CurrentThread.ManagedThreadId}");
+        throw new Exception();
+        Thread.Sleep(5000);
+        Console.WriteLine($"Example1 закончил работать в {Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    private static void Example2(object arg)
+    {
+        Console.WriteLine($"Example2 начал работать в {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(2000);
+        Console.WriteLine($"Example2 закончил работать в {Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    private static void Report()
+    {
+        ThreadPool.GetMaxThreads(out int maxWorkerThreads, out int maxPortThreads);
+        ThreadPool.GetAvailableThreads(out int availableWorkerThreads, out int availablePortThreads);
+
+        Console.WriteLine($"Рабочие потоки {availableWorkerThreads} из {maxWorkerThreads}");
+    }
+    private static void WriteChar(object arg) 
+    { 
+        Console.WriteLine($"WriteChar начал работать в {Thread.CurrentThread.ManagedThreadId}"); 
+        char item = (char)arg; 
+        for (int i = 0; i < 120; i++) 
+        { 
+            Console.Write(item); 
+            Thread.Sleep(50); 
+        } 
+    }
+}
---
+++ ConsoleApp6/ThreadPoolWorker.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApp6
+{
+    public class ThreadPoolWorker<TResult>
+    {
+        private readonly Func<object, TResult> _func;
+        private TResult _result;
+
+        public bool Completed { get; private set; } = false;
+        public bool IsSuccess { get; private set; } = false;
+
+        public TResult Result
+        {
+            get
+            {
+                while (Completed == false)
+                {
+                    Thread.Sleep(50);
+                }
+                return IsSuccess == true ? _result : throw new Exception();
+            }
+        }
+
+        public ThreadPoolWorker(Func<object, TResult> func)
+        {
+            _func = func ?? throw new ArgumentNullException();
+        }
+
+        public void Start(object state)
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadExecute), state);
+        }
+
+        private void ThreadExecute(object state)
+        {
+            try
+            {
+                _result = _func.Invoke(state);
+                IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                IsSuccess = false;
+            }
+            finally
+            {
+                Completed = true;
+            }
+        }
+    }
+}
